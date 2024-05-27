f38bafb4322671b2cd07cbd1b7fd5e14df08342d
Austin Lindenberg <austin@appsumo.com>
2024-05-27 15:06:53 -0700

    fix img path

--- src/components/SharkTile.tsx
+++ src/components/SharkTile.tsx
@@ -13,7 +13,7 @@ export default function SharkTile(shark: SharkType) {
             href={`/sharks/${shark.name.toLowerCase().replace(' ', '_')}`}
             className="border-2 border-blue-300 text-center p-2 transform transition duration-500 ease-in-out hover:scale-105">
             <Image
-                src={`/sharks/${shark.name.toLowerCase()}/index.jpg`}
+                src={`/sharks/${shark.name.toLowerCase().replace(' ', '_')}/index.jpg`}
                 alt={shark.name}
                 width={300}
                 height={200}
