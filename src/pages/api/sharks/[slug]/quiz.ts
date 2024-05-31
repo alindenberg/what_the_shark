25f14dd767930009282d5e00dadab2e73e9ad36e
Austin Lindenberg <austin@appsumo.com>
2024-05-31 10:53:18 -0700

    make server url env var

--- src/pages/api/sharks/[slug]/quiz.ts
+++ src/pages/api/sharks/[slug]/quiz.ts
@@ -9,7 +9,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>,
 ) {
-    await fetch(`http://127.0.0.1:8000/sharks/${req.query.slug}/quiz/`)
+    await fetch(`${process.env.SHARKS_URL}/sharks/${req.query.slug}/quiz/`)
         .then(response => response.json())
         .then(data => res.status(200).json(data))
         .catch(err => { console.error("error getting quiz ", err); res.status(400).json({ name: 'error' }) });
