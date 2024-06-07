f26d7c59d3bb2223babcb7aefcfa6df0a8630916
Austin Lindenberg <austin@appsumo.com>
2024-06-07 08:04:21 -0700

    WIP

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,27 @@
-import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import sharks from '@/data/sharks.json';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const handleSharkChange = (event) => {
+    const sharkSlug = event.target.value;
+    router.push(`/sharks/${sharkSlug}`);
   };
+
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-600 p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Link aria-disabled={true} href="/">Home</Link>
+        <select onChange={handleSharkChange}>
+          {sharks.map((shark) => (
+            <option key={shark.id} value={shark.name.toLowerCase().replace(' ', '_')}>
+              {shark.name}
+            </option>
+          ))}
+        </select>
       </div>
     </nav>
   );
