122436f73a3d3912767fd492679f35b45a8f1490
Austin Lindenberg <austin@appsumo.com>
2024-08-04 10:25:38 -0700

    preload shark carousel images

--- src/components/SharkCarousel.tsx
+++ src/components/SharkCarousel.tsx
@@ -1,14 +1,22 @@
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
-import { useState } from "react";
 import SharkType from "@/types/SharkType";
 
 
 export default function SharkCarousel(shark: SharkType) {
     const [imgIndex, setImgIndex] = useState(0);
     let imgNames = ['index.jpg', 'carousel_1.jpg', 'carousel_2.jpg', 'carousel_3.jpg', 'carousel_4.jpg', 'carousel_5.jpg']
+
+    // useEffect(() => {
+    //     // Preload images
+    //     // imgNames.forEach((imgName) => {
+    //     //     const img = new Image();
+    //     //     img.src = `/sharks/${shark.name.toLowerCase().replace(' ', '_')}/${imgName}`;
+    //     // });
+    // }, [shark.name, imgNames]);
 
     const handleLeftClick = (event: React.MouseEvent) => {
         event.preventDefault();
@@ -35,6 +43,7 @@ export default function SharkCarousel(shark: SharkType) {
                     width={500}
                     height={400}
                     style={{ borderRadius: '50%' }}
+                    rel={imgIndex !== 0 ? 'preload' : undefined}
                     loading={imgIndex === 0 ? 'eager' : 'lazy'}
                 />
                 <FontAwesomeIcon
